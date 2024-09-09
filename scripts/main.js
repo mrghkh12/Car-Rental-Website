0f40d489bd43fcb144757ffbef4ab8fb76e81210
mohammad reza <mrghkh12@gmail.com>
2024-09-09 23:53:51 +0330

    feat(main.js): add scroll reveal lib & set scroll efct for header section

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,26 @@ navLinks.addEventListener('click', e =>{
     navLinks.classList.remove('open');
     menuBtnIcon.className = 'ri-menu-line'
 })
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration:1000,
+}
+
+ScrollReveal().reveal('.header__image img', {
+    ...scrollRevealOption,
+    origin:"right",
+})
+ScrollReveal().reveal('.header__content h1', {
+    ...scrollRevealOption,
+    delay:500,
+})
+ScrollReveal().reveal('.header__content p', {
+    ...scrollRevealOption,
+    delay:1000,
+})
+ScrollReveal().reveal('.header__links', {
+    ...scrollRevealOption,
+    delay:1500,
+})
